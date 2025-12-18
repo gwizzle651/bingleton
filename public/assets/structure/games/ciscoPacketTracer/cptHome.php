1796a66f12aa4f9e526b52d4f73d2307b5e53d60
JimBobJoe36 <188243628+JimBobJoe36@users.noreply.github.com>
2025-12-18 12:43:01 -0600

    Updated small descriptions to pages

--- public/assets/structure/games/ciscoPacketTracer/cptHome.php
+++ public/assets/structure/games/ciscoPacketTracer/cptHome.php
@@ -1,1 +1,13 @@
-# for the home page of the game cisco pcaket tracer. the user should be able to access all categories and challenges from here.
+<?php
+$title = "Cisco Packet Tracer home"
+?>
+
+<DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?php echo $title ?></title>
+    </head>
+    <body>
+        <h1><?php echo $title ?></h1>
+    </body>
+</html>
