c70cb3c6b5e3084267263c4b172cfa5b1ed7ca13
JimBobJoe36 <188243628+JimBobJoe36@users.noreply.github.com>
2025-12-18 12:43:01 -0600

    Updated small descriptions to pages

--- public/assets/structure/games/excel/excelHome.php
+++ public/assets/structure/games/excel/excelHome.php
@@ -1,1 +1,12 @@
-# home for excel page
+<?php 
+$title = "Excel Speedrun Home";
+?>
+<DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?php echo $title ?></title>
+    </head>
+    <body>
+        <h1><?php echo $title ?></h1>
+    </body>
+</html>
